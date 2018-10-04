b9c61fad8a9bf8b013d9a8de1e468c7b762b3b49
Rod Johnson <rod@atomist.com>
2018-10-04 11:52:23 -0400

    Customize

--- lib/machine/machine.ts
+++ lib/machine/machine.ts
@@ -1,42 +1,19 @@
-/*
- * Copyright © 2018 Atomist, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import {
     AutoCodeInspection,
     Autofix,
-    AutofixRegistration,
     Build,
     GitHubRepoRef,
     goalContributors,
     goals,
-    hasFile,
-    not,
     onAnyPush,
     PushImpact,
     SoftwareDeliveryMachine,
     SoftwareDeliveryMachineConfiguration,
     whenPushSatisfies,
 } from "@atomist/sdm";
-import {
-    createSoftwareDeliveryMachine,
-    summarizeGoalsInGitHubStatus,
-} from "@atomist/sdm-core";
+import { createSoftwareDeliveryMachine, summarizeGoalsInGitHubStatus, } from "@atomist/sdm-core";
 import { codeMetrics } from "@atomist/sdm-pack-sloc";
 import {
-    CloudNativeGitHubIssueRaisingReviewListener,
     HasSpringBootApplicationClass,
     HasSpringBootPom,
     IsMaven,
@@ -47,11 +24,9 @@ import {
     SetAtomistTeamInApplicationYml,
     SpringProjectCreationParameterDefinitions,
     SpringProjectCreationParameters,
-    SpringStyleGitHubIssueRaisingReviewListener,
     springSupport,
     TransformSeedToCustomProject,
 } from "@atomist/sdm-pack-spring";
-import axios from "axios";
 
 export function machine(
     configuration: SoftwareDeliveryMachineConfiguration,
@@ -59,16 +34,17 @@ export function machine(
 
     const sdm: SoftwareDeliveryMachine = createSoftwareDeliveryMachine(
         {
-            name: "Spring software delivery machine",
+            name: "Zeus software delivery machine",
             configuration,
         });
 
-    const autofix = new Autofix().with(AddLicenseFile);
+    const autofix = new Autofix();
     const inspect = new AutoCodeInspection();
+    const pushImpact = new PushImpact();
 
     const checkGoals = goals("checks")
         .plan(inspect)
-        .plan(new PushImpact())
+        .plan(pushImpact)
         .plan(autofix);
 
     const buildGoals = goals("build")
@@ -94,8 +70,6 @@ export function machine(
             },
             autofix: {},
             reviewListeners: [
-                CloudNativeGitHubIssueRaisingReviewListener,
-                SpringStyleGitHubIssueRaisingReviewListener,
             ],
         }),
         codeMetrics(),
@@ -114,19 +88,6 @@ export function machine(
         ],
     });
 
-    sdm.addGeneratorCommand<SpringProjectCreationParameters>({
-        name: "create-kotlin-spring",
-        intent: "create kotlin spring",
-        description: "Create a new Kotlin Spring Boot REST service",
-        parameters: SpringProjectCreationParameterDefinitions,
-        startingPoint: GitHubRepoRef.from({ owner: "atomist-seeds", repo: "kotlin-rest", branch: "master" }),
-        transform: [
-            ReplaceReadmeTitle,
-            SetAtomistTeamInApplicationYml,
-            TransformSeedToCustomProject,
-        ],
-    });
-
     sdm.addCommand(ListBranchDeploys);
 
     // Manages a GitHub status check based on the current goals
@@ -134,14 +95,3 @@ export function machine(
 
     return sdm;
 }
-
-export const LicenseFilename = "LICENSE";
-
-export const AddLicenseFile: AutofixRegistration = {
-    name: "License Fix",
-    pushTest: not(hasFile(LicenseFilename)),
-    transform: async p => {
-        const license = await axios.get("https://www.apache.org/licenses/LICENSE-2.0.txt");
-        return p.addFile(LicenseFilename, license.data);
-    },
-};
